d604bdf2968dee369f8433ace1028f8b9c84d7b5
Emile 'iMil' Heitor <imil@NetBSD.org>
2015-12-31 15:25:58 +0000

    split platform-related variables

--- platform.sh
+++ platform.sh
@@ -1,6 +1,7 @@
 pkgin=`which pkgin`
-pax="`which pax` -rwpe"
-rsync="`which rsync` -av"
+pax="${debug}`which pax` -rwpe"
+rsync="${debug}`which rsync` -av"
+cp=`${debug}which cp`
 pkg_info=`which pkg_info`
 awk=`which awk`
 sort=`which sort`
@@ -25,4 +26,4 @@ esac
 # binaries needed by pkg_install and not listed in +INSTALL
 def_bins="${def_bins} /usr/sbin/pwd_mkdb ${useradd} ${groupadd} \
 	${pkg_info} ${pkgin} /bin/sh /bin/test /sbin/nologin /bin/echo \
-	/bin/ps /bin/sleep"
+	/bin/ps /bin/sleep `which sysctl`"
--- sailor.sh
+++ sailor.sh
@@ -1,11 +1,18 @@
 #!/bin/sh
 
-[ $# -lt 1 ] && echo "usage: $0 <ship.conf>" && exit 1
+usage()
+{
+	echo "usage: $0 <build|start|stop> <ship.conf> [debug]" && exit 1
+}
 
-. ${1}
+[ $# -lt 2 ] && usage
+
+cf=${2}
+
+[ $# -gt 2 ] && debug="`which echo` "
+
+. ${cf}
 . ./platform.sh
-
-[ ! -d ${shippath} ] && mkdir -p ${shippath}
 
 reqs=""
 
@@ -45,56 +52,6 @@ pkg_requires()
 	[ ! -z "${reqs}" ] && sync_reqs ${1}
 }
 
-# install wanted binaries
-prefix=`${pkg_info} -QLOCALBASE pkgin`
-varbase=`${pkg_info} -QVARBASE pkgin`
-for bin in ${def_bins} ${shipbins}
-do
-	bin_requires ${bin}
-done
-
-# devices
-mkdir -p ${shippath}/dev
-cp /dev/MAKEDEV ${shippath}/dev
-cd ${shippath}/dev && sh MAKEDEV std
-cd -
-
-# tmp directory
-mkdir -p ${shippath}/tmp
-chmod 1777 ${shippath}/tmp
-
-# needed for pkg_install / pkgin to work
-for d in db/pkg db/pkgin log run tmp
-do
-	mkdir -p ${shippath}/${varbase}/${d}
-done
-
-${pax} ${prefix}/etc/pkgin ${shippath}/
-
-# raw pkg_install / pkgin installation
-pkg_requires pkg_install
-for p in pkg_install pkgin
-do
-	pkg_tarup -d ${shippath}/tmp ${p}
-	${tar} zxfp ${shippath}/tmp/${p}*tgz -C ${shippath}/${prefix}
-	# install pkg{_install,in} the right way
-done
-chroot ${shippath} ${prefix}/sbin/pkg_add /tmp/pkg_install*tgz
-
-# minimal etc provisioning
-mkdir -p ${shippath}/etc
-cp /usr/share/zoneinfo/GMT ${shippath}/etc/localtime
-cp /etc/resolv.conf ${shippath}/etc/
-# custom /etc
-common="ships/common/${OS}"
-# populate commons
-${rsync} ${common}/ ${shippath}/
-# populate 3rd party
-${rsync} ships/${shipname}/ ${shippath}/
-# fix etc perms
-${chown} -R root:wheel ${shippath}/etc
-${chmod} 600 ${shippath}/etc/master.passwd
-
 # extract needed tools from pkg_add install script
 need_tools()
 {
@@ -108,22 +65,92 @@ need_tools()
 	done
 }
 
-need_tools pkgin
+build()
+{
+	[ ! -d ${shippath} ] && mkdir -p ${shippath}
 
-${pkgin} -y -c ${shippath} update
+	# install wanted binaries
+	prefix=`${pkg_info} -QLOCALBASE pkgin`
+	varbase=`${pkg_info} -QVARBASE pkgin`
 
-for pkg in ${packages}
-do
-	# retrieve dependencies names
-	pkg_reqs="`${pkgin} -P -c ${shippath} sfd ${pkg} | \
-		awk '/^\t/ {print $1}'` ${pkg}"
-	for p in ${pkg_reqs}
+	for bin in ${def_bins} ${shipbins}
 	do
-		# install all dependencies requirements
-		pkg_requires ${p}
+		bin_requires ${bin}
 	done
-	PKG_RCD_SCRIPTS=yes ${pkgin} -y -c ${shippath} install ${pkg}
-	${pkgin} -y clean
-done
+	
+	# devices
+	mkdir -p ${shippath}/dev
+	${cp} /dev/MAKEDEV ${shippath}/dev
+	cd ${shippath}/dev && sh MAKEDEV std
+	cd -
+	
+	# tmp directory
+	mkdir -p ${shippath}/tmp
+	chmod 1777 ${shippath}/tmp
+	
+	# needed for pkg_install / pkgin to work
+	for d in db/pkg db/pkgin log run tmp
+	do
+		mkdir -p ${shippath}/${varbase}/${d}
+	done
+	
+	${pax} ${prefix}/etc/pkgin ${shippath}/
+	
+	# raw pkg_install / pkgin installation
+	pkg_requires pkg_install
+	for p in pkg_install pkgin
+	do
+		pkg_tarup -d ${shippath}/tmp ${p}
+		${tar} zxfp ${shippath}/tmp/${p}*tgz -C ${shippath}/${prefix}
+		# install pkg{_install,in} the right way
+	done
+	chroot ${shippath} ${prefix}/sbin/pkg_add /tmp/pkg_install*tgz
+	
+	# minimal etc provisioning
+	mkdir -p ${shippath}/etc
+	${cp} /usr/share/zoneinfo/GMT ${shippath}/etc/localtime
+	${cp} /etc/resolv.conf ${shippath}/etc/
+	# custom /etc
+	common="ships/common/${OS}"
+	# populate commons
+	${rsync} ${common}/ ${shippath}/
+	# populate 3rd party
+	${rsync} ships/${shipname}/ ${shippath}/
+	# fix etc perms
+	${chown} -R root:wheel ${shippath}/etc
+	${chmod} 600 ${shippath}/etc/master.passwd
+	
+	need_tools pkgin
+	
+	${pkgin} -y -c ${shippath} update
+	
+	for pkg in ${packages}
+	do
+		# retrieve dependencies names
+		pkg_reqs="`${pkgin} -P -c ${shippath} sfd ${pkg} | \
+			awk '/^\t/ {print $1}'` ${pkg}"
+		for p in ${pkg_reqs}
+		do
+			# install all dependencies requirements
+			pkg_requires ${p}
+		done
+		PKG_RCD_SCRIPTS=yes ${pkgin} -y -c ${shippath} install ${pkg}
+		${pkgin} -y clean
+	done
+	
+	echo "${service}=YES" >> ${shippath}/etc/rc.conf
+}
 
-echo "${service}=YES" >> ${shippath}/etc/rc.conf
+case ${1} in
+build|create|make)
+	build
+	;;
+start|stop)
+	chroot ${shippath} /etc/rc.d/${service} ${1}
+	;;
+*)
+	usage
+	;;
+esac
+
+exit 0
