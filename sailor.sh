db7f7c4226cf0220f202a7a5e75815dbbea9ca71
Emile 'iMil' Heitor <imil@NetBSD.org>
2015-12-31 13:31:33 +0000

    1st fully working copy on NetBSD

--- sailor.sh
+++ sailor.sh
@@ -10,7 +10,18 @@ rsync="`which rsync` -av"
 pkg_info=`which pkg_info`
 awk=`which awk`
 sort=`which sort`
-sed=`which sed`
+grep=`which egrep`
+tar=`which tar`
+OS=`uname -s`
+
+case $OS in
+*arwin*)
+	ldd=`which otool`
+	;;
+*BSD)
+	ldd=`which ldd`
+	;;
+esac
 
 [ ! -d ${shippath} ] && mkdir -p ${shippath}
 
@@ -18,9 +29,9 @@ reqs=""
 
 link_target()
 {
-	for lib in ${reqs}
+	for lnk in ${reqs}
 	do
-		[ -h ${lib} ] && reqs="${reqs} `readlink -f ${lib}`"
+		[ -h ${lnk} ] && reqs="${reqs} `readlink -f ${lnk}`"
 	done
 }
 
@@ -35,9 +46,12 @@ sync_reqs()
 
 bin_requires()
 {
-	reqs=`ldd -f'%p\n' ${1}`
-
-	[ ! -z "${reqs}" ] && sync_reqs ${1}
+	# grep link matches both symlinks and ELF executables ;)
+	if [ ! -z "`file ${1}|${grep} 'link'`" ]; then
+		reqs="`${ldd} -f'%p\n' ${1}` ${1}"
+	
+		[ ! -z "${reqs}" ] && sync_reqs ${1}
+	fi
 	${pax} ${1} ${shippath}/
 }
 
@@ -52,9 +66,11 @@ pkg_requires()
 # install wanted binaries
 prefix=`${pkg_info} -QLOCALBASE pkgin`
 varbase=`${pkg_info} -QVARBASE pkgin`
-def_bins="${prefix}/bin/pkgin ${prefix}/sbin/pkg_info \
+# /bin/sh is needed by pkg_install
+def_bins="${prefix}/bin/pkgin ${prefix}/sbin/pkg_info /bin/sh \
 	/libexec/ld.elf_so /usr/libexec/ld.elf_so \
-	/usr/sbin/pwd_mkdb"
+	/usr/sbin/pwd_mkdb /usr/sbin/useradd /usr/sbin/groupadd \
+	/bin/test /sbin/nologin /bin/echo /bin/ps /bin/sleep"
 for bin in ${def_bins} ${shipbins}
 do
 	bin_requires ${bin}
@@ -71,7 +87,7 @@ mkdir -p ${shippath}/tmp
 chmod 1777 ${shippath}/tmp
 
 # needed for pkg_install / pkgin to work
-for d in db/pkg db/pkgin log run
+for d in db/pkg db/pkgin log run tmp
 do
 	mkdir -p ${shippath}/${varbase}/${d}
 done
@@ -83,7 +99,7 @@ pkg_requires pkg_install
 for p in pkg_install pkgin
 do
 	pkg_tarup -d ${shippath}/tmp ${p}
-	tar zxfp ${shippath}/tmp/${p}*tgz -C ${shippath}/${prefix}
+	${tar} zxfp ${shippath}/tmp/${p}*tgz -C ${shippath}/${prefix}
 	# install pkg{_install,in} the right way
 done
 chroot ${shippath} ${prefix}/sbin/pkg_add /tmp/pkg_install*tgz
@@ -92,24 +108,32 @@ chroot ${shippath} ${prefix}/sbin/pkg_add /tmp/pkg_install*tgz
 mkdir -p ${shippath}/etc
 cp /usr/share/zoneinfo/GMT ${shippath}/etc/localtime
 cp /etc/resolv.conf ${shippath}/etc/
-${rsync} etc/ ${shippath}/etc/
+# custom /etc
+common="ships/common/${OS}"
+# populate commons
+${rsync} ${common}/ ${shippath}/
 # populate 3rd party
 ${rsync} ships/${shipname}/ ${shippath}/
-
-${pkgin} -y -c ${shippath} update
-
-#touch ${shippath}/etc/master.passwd /etc/pwd.db
-#chroot ${shippath} /usr/sbin/pwd_mkdb /etc/passwd
+# fix etc perms
+${chown} -R root:wheel ${shippath}/etc
+${chmod} 600 ${shippath}/etc/master.passwd
 
 # extract needed tools from pkg_add install script
-tools="`${pkg_info} -i pkgin | \
-	${awk} -F= '/^[^\=]+="\// {print $2}' | \
-	${sed} -E 's/[\"\ \t]//g' | ${sort} -u`"
+need_tools()
+{
+	tools="`${pkg_info} -i ${1} | \
+		${awk} -F= '/^[^\=]+="\// {print $2}' | \
+		${grep} -o '/[^\"\ ]+' | ${sort} -u`"
+	
+	for t in ${tools}
+	do
+		[ -f ${t} -a -x ${t} ] && bin_requires ${t}
+	done
+}
 
-for t in ${tools}
-do
-	[ -f ${t} -a -x ${t} ] && bin_requires ${t}
-done
+need_tools pkgin
+
+${pkgin} -y -c ${shippath} update
 
 for pkg in ${packages}
 do
@@ -121,7 +145,8 @@ do
 		# install all dependencies requirements
 		pkg_requires ${p}
 	done
-	${pkgin} -y -c ${shippath} install ${pkg}
+	PKG_RCD_SCRIPTS=yes ${pkgin} -y -c ${shippath} install ${pkg}
+	${pkgin} -y clean
 done
 
 echo "${service}=YES" >> ${shippath}/etc/rc.conf
