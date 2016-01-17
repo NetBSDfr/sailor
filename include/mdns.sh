8c931674db2bbe0ec1c69989a4a7df5441ce29d9
Emile 'iMil' Heitor <imil@NetBSD.org>
2016-01-17 15:47:26 +0000

    ping inside ship

--- include/mdns.sh
+++ include/mdns.sh
@@ -75,7 +75,8 @@ mdns()
 	# wait for name resolution to be ready
 	[ "${action}" = "add" ] && while :
 		do
-			${ping} -c 1 localhost >/dev/null 2>&1
+			chroot ${shippath} \
+				${ping} -c 1 localhost >/dev/null 2>&1
 			[ $? -eq 0 ] && break
 			echo "waiting for resolver..."
 			sleep 1
